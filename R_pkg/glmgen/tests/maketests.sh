47bfe51db54f8d7e39440c2b0a9ab79638b7c9f9
capolitsch <capolitsch@cmu.edu>
2021-11-18 16:52:24 -0500

    Strip out more unneeded functionality

--- R_pkg/glmgen/tests/maketests.sh
+++
@@ -1,8 +0,0 @@
-
-
-ROOT=../../
-cd $ROOT && \
-sudo R CMD INSTALL glmgen && cd glmgen && \
-cp src/glmgen.so src/libglmgen.so && cd tests && \
-gcc -g ctest.c -I../inst/include/ -L../src/ -lm -lglmgen
-#LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ROOT/src
