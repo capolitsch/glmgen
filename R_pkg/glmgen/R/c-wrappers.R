c453aaf2d49728cc2e968b0b420618236ebe16b7
capolitsch <capolitsch@cmu.edu>
2021-12-07 19:46:06 -0500

    Small tweaks to wrappers

--- R_pkg/glmgen/R/c-wrappers.R
+++ R_pkg/glmgen/R/c-wrappers.R
@@ -88,7 +88,7 @@
                         lambda,
                         x_eval,
                         fitted_values,
-                        zero_tol = 1e-16,
+                        zero_tol,
                         ...) {
   p <- .Call("tf_predict_R",
     sX = as.double(obj$x / obj$scale["x"]),
